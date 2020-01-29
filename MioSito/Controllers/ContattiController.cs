16a8228536b85296649434c7e0cb5adeed5bb8f6
l.carpinelli <l.carpinelli@VETRYA-PC100>
2020-01-29 18:13:24 +0100

    Add project files.

---
+++ MioSito/Controllers/ContattiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MioSito.Models;
+
+namespace MioSito.Controllers
+{
+    public class ContattiController : Controller
+    {
+        public IActionResult Index()
+        {
+            Contatti Contatto= new Contatti();
+
+            return View(Contatto);
+        }
+    }
+}
